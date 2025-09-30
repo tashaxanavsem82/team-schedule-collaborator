16f1f46b4c6885a516ee9270172ad2d454aa1a2b
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-30 09:54:32 -0500

    Fix MongoDB connection warnings by updating mongoose options.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/team-schedule', { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 10000, useFindAndModify: false })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/team-schedule', { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 10000, useFindAndModify: false, useCreateIndex: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
