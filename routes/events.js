8df13798fb90abda0b761449c21fdc563cc8cd18
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-30 09:53:03 -0500

    feat: initial commit for routes/events.js

---
+++ routes/events.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Event = require('../models/Event');
+const router = express.Router();
+
+// Create an event
+router.post('/', async (req, res) => {
+  const newEvent = new Event(req.body);
+  try {
+    const savedEvent = await newEvent.save();
+    res.status(200).json(savedEvent);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get events
+router.get('/', async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.status(200).json(events);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
