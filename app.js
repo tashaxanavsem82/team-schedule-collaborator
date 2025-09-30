1053394d9f52e42eb86cacd28631eeaf61bd0f73
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-30 09:56:13 -0500

    Fix formatting and add HTTP header for security.

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const eventsRoute = require('./routes/events');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+});
 app.use('/api/events', eventsRoute);
 
 module.exports = app;
